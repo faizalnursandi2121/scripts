2563321798277426032468561e109e991da36290
faizalnursandi2121 <faizalnursandi21@gmail.com>
2025-12-31 22:07:10 +0700

    Add script loader for specific game ID

---
+++ chloe.lua
@@ -0,0 +1,12 @@
+local games = {
+    [121864768012064] = "[121864768012064] = "https://raw.githubusercontent.com/faizalnursandi2121/scripts/main/FishIt.lua",",
+}
+
+local currentID = game.PlaceId
+local scriptURL = games[currentID]
+
+if scriptURL then
+    loadstring(game:HttpGet(scriptURL))()
+else
+    game.Players.LocalPlayer:Kick("Yo! This game ain't on the list.\nCheck the Discord for whitelisted games, homie.")
+end
